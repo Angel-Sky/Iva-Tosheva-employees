f399ee88e92de00f61ea66dfe57b7c9e9b6bfedd
Iva Tosheva <otevi55@gmail.com>
2022-05-24 21:54:18 +0300

    Filter projects with common max worked days

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ function solve() {
             let reader = new FileReader();
             reader.onload = function (e) {
                 fileData = e.target.result.split("\n");
-                // console.log(fileData)
-                findWorkingDays(formatData(fileData));
-                renderData(fileData);
+                renderData(filterProjects(fileData));
             }
             reader.readAsText(uploadedFile.files[0]);
         } else {
@@ -49,7 +47,7 @@ function solve() {
         return projects;
     }
 
-    function findWorkedDays(data) {
+    function findCommonDays(data) {
         for (let project of data) {
             let [empl1ID, startOne, endOne, empl2ID, startTwo, endTwo] = project.data;
             let diff = 0;
@@ -57,10 +55,10 @@ function solve() {
             endOne = Date.parse(endOne) / 1000;
             startTwo = Date.parse(startTwo) / 1000;
             endTwo = Date.parse(endTwo) / 1000;
-        
+
             if ((startTwo >= startOne && startTwo <= endOne)) {
                 if (endTwo > endOne) {
-                   diff = endOne - startTwo
+                    diff = endOne - startTwo
                 } else {
                     diff = endTwo - startTwo
                 }
@@ -73,20 +71,37 @@ function solve() {
             }
 
             diff = (Math.ceil(diff / 86400));
-            project.workDays = diff;
+            project.workedDays = diff;
         }
         return data;
     }
 
-    function renderData(data) {
-        for (let i = 0; i < data.length; i++) {
-            let row = table.insertRow(-1);
-            let cells = data[i].split(",");
-            for (let j = 0; j < cells.length; j++) {
-                let cell = row.insertCell(-1);
-                cell.innerHTML = cells[j];
+    function filterProjects(fileData) {
+        let data = findCommonDays(formatData(fileData))
+        let arrMostCommonDaysProjects = [];
+        let arrOfData = Object.entries(data)
+            .sort((a, b) => (b[1].workedDays - a[1].workedDays))
+
+        for (let i in arrOfData) {
+            if (Number(i) + 1 < arrOfData.length &&
+                arrOfData[i][1].workedDays >= arrOfData[Number(i) + 1][1].workedDays) {
+                    arrMostCommonDaysProjects.push(arrOfData[i])
             }
         }
+
+        return arrMostCommonDaysProjects;
+    }
+
+    function renderData(data) {
+        console.log(data)
+        // for (let i = 0; i < data.length; i++) {
+        //     let row = table.insertRow(-1);
+        //     let cells = data[i].split(",");
+        //     for (let j = 0; j < cells.length; j++) {
+        //         let cell = row.insertCell(-1);
+        //         cell.innerHTML = cells[j];
+        //     }
+        // }
         output.innerHTML = "";
         output.appendChild(table);
     }
