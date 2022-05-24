21a35a7ce854f58d5f8e6cc004c7aade00cc6c88
Iva Tosheva <otevi55@gmail.com>
2022-05-24 15:55:15 +0300

    Print all the data from the file as a table

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 function solve() {
     let uploadedFile = document.querySelector("#employeesUpload");
+    let fileData;
+    let table = document.createElement("table");
+    let output = document.getElementById("result");
     document.querySelector("#btn-submit-employees").addEventListener("click", addEvent);
 
     function addEvent(e) {
@@ -8,12 +11,27 @@ function solve() {
         if (regex.test(uploadedFile.value.toLowerCase())) {
             let reader = new FileReader();
             reader.onload = function (e) {
-                let fileData = e.target.result.split("\n");
+                fileData = e.target.result.split("\n");
                 console.log(fileData)
+                renderData(fileData);
             }
             reader.readAsText(uploadedFile.files[0]);
         } else {
             alert("Please upload a valid CSV file.");
         }
+    }
+
+    function renderData(data) {
+        for (let i = 0; i < data.length; i++) {
+            let row = table.insertRow(-1);
+            let cells = data[i].split(",");
+            console.log(cells)
+            for (let j = 0; j < cells.length; j++) {
+                let cell = row.insertCell(-1);
+                cell.innerHTML = cells[j];
+            }
+        }
+        output.innerHTML = "";
+        output.appendChild(table);
     }
 }
